ecfed9b019d871a88b5b8321ea917005151f757e
hloan3101 <hloan975@gmail.com>
2022-11-09 20:44:48 +0700

    home

---
+++ app/src/main/java/com/example/notemanagerapp/ui/fragment/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.example.notemanagerapp.ui.fragment;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.notemanagerapp.R;
+import com.example.notemanagerapp.ui.viewmodel.HomeViewModel;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel mViewModel;
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home2, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
